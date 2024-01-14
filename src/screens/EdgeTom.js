5bd64a848bc170d173d776b7f787b732a6f29e71
dell <arjunpathak164@gmail.com>
2024-01-14 19:16:55 +0530

    testing

--- src/screens/EdgeTom.js
+++ src/screens/EdgeTom.js
@@ -3,20 +3,70 @@ import './EdgeTom.css'
 import Nav from '../Nav'
 import {motion as m} from 'framer-motion'
 import Banner from '../Banner'
+import Row from '../Row'
+import requests from '../Requests';
+
 
 function EdgeTom() {
+
+    const containerStyle = {
+        position: 'relative',
+        display: 'inline-block',
+      };
+    
+      const overlayStyle = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        background: 'linear-gradient(to right, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0) 100%)',
+        pointerEvents: 'none', // This allows clicks to pass through the overlay to the underlying content
+      };
   return (
     <m.div initial={{opacity:0}}
     animate={{opacity:1}}
     transition={{duration: 0.75, ease:"easeOut"}}
     className='edgetom'>
-        <Nav/>
-        <h1>hello</h1>
         
+        <Nav/>
+        <div className='edge' >
+        <div className='edge__contents' >
+            <div className='content--fade'>
 
-        <div className='edge__gradient'>
+            </div>
+            <h1 className='edge__title'>
+                Edge Of Tomorrow
+            </h1>
+            <div className='edge__buttons'>
+                <button className='edge__button'>Play</button>
+                
 
+            </div>
+            <h1 className='edge__description'>
+            After the alien forces 
+            launch an attack on 
+            Earth and threaten to 
+            wipe out the human race, 
+            Captain Cage, who is trapped in 
+            a time loop, teams up 
+            with a warrior to save the planet.
+                
+                
+            </h1>
+            
         </div>
+
+        <div className='edge--fadeBottom'>
+            
+        </div>
+      
+    </div>
+    <Row
+        title="Netflix Orignials"
+        fetchUrl={requests.fetchNetflixOriginals}
+        
+        />
 
         
       
